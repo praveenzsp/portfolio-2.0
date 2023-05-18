c1ae183c5988dbee58a53ce06c6f2191a254add7
praveenzsp <psai6645@gmail.com>
2023-05-18 15:36:13 +0530

    fixed bugs

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className='w-full flex lg:justify-between flex-col items-center lg:flex-row'>
         <div className='lg:px-52 lg:py-10 text-center py-10'>
           <h1 className='lg:text-[40px] font-bold tracking-wide text-[25px]'>Let's grab a boba </h1>
-          <p className='tracking-wide text-[12px] lg:text-[15px]'>Shoot me an email at <span className='text-primary text-[12px]'>psai6645@gmail.com</span></p>
+          <p className='tracking-wide text-[12px] lg:text-[15px] text-start'>Shoot me an email at <span className='text-primary text-[12px]'>psai6645@gmail.com</span></p>
         </div>
         <div className='lg:pr-52 lg:p-10 pb-6'>
           <ul>
